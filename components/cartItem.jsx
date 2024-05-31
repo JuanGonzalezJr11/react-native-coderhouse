e685d796a4170641271851f45656ac3d3b1aa4c2
Juan Gonzalez <juangonzalez.coderhouse@gmail.com>
2024-05-30 23:42:14 -0300

    Se agrego la opción de remover items del carrito

--- components/cartItem.jsx
+++ components/cartItem.jsx
@@ -1,37 +1,138 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { colors } from '../constants/colors'
+import {
+  Animated,
+  Easing,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { colors } from "../constants/colors";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useDispatch } from "react-redux";
+import { removeCartItem } from "../features/cartSlice";
+import ModalDeleteItem from "./modalDeleteItem";
+import { useFocusEffect } from "@react-navigation/native";
 
 const CartItem = ({ cartItem }) => {
+  const [options, setOptions] = useState(false);
+  const dispatch = useDispatch();
+  const slideAnimation = useRef(new Animated.Value(0)).current;
+  const [modalVisible, setModalVisible] = useState(false);
+  const visibleOption = () => {
+    setOptions(!options);
+  };
+  const slideInterpolate = slideAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [100, 0],
+  });
+  const showModalDelete = () => {
+    setModalVisible(true);
+  };
+  const handleDelete = () => {
+    dispatch(removeCartItem(cartItem))
+    setModalVisible(false)
+  }
+  const handleCancel = () => {
+    setModalVisible(false)
+    console.log(options)
+  }
+  useEffect(() => {
+    Animated.timing(slideAnimation, {
+      toValue: options ? 1 : 0,
+      duration: 300,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start();
+  }, [options]);
+  // useFocusEffect(() => {
+  //   React.useCallback(() => {
+  //     setOptions(false)
+  //   })
+  // }, [])
   return (
-    <View style={styles.card} onPress={() => { }}>
-      <View style={styles.textContainer}>
-        <Text style={styles.textTitle}>{cartItem.title} x{cartItem.quantity}</Text>
-        <Text style={styles.textPrice}>${cartItem.price}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.card} onPress={() => {}}>
+        <View style={styles.container}>
+          <Pressable
+            style={styles.textPressable}
+            onPress={visibleOption}
+            onBlur={() => setOptions(false)}
+          >
+            <Text style={styles.textTitle}>
+              {cartItem.title} x{cartItem.quantity}
+            </Text>
+            <Text style={styles.textPrice}>${cartItem.price}</Text>
+          </Pressable>
+        </View>
       </View>
+      <Animated.View
+        style={[
+          styles.optionsContainer,
+          { transform: [{ translateX: slideInterpolate }] },
+        ]}
+      >
+        <Pressable style={styles.deleteButton} onPress={showModalDelete}>
+          <AntDesign name="delete" size={28} color={colors.white} />
+        </Pressable>
+      </Animated.View>
+      <ModalDeleteItem
+        modalVisible={modalVisible}
+        item={cartItem.title}
+        quantity={cartItem.quantity}
+        handleCancel={handleCancel}
+        handleDelete={handleDelete}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    width: "100%"
+  },
   card: {
     borderColor: colors.primary,
     borderBottomWidth: 1,
     marginHorizontal: 10,
-    marginVertical: 5
-  },
-  textContainer: {
-    padding: 10,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  container: {
+    padding: 10,
+    width: "100%",
+    flexDirection: "column",
+  },
+  textPressable: {
+    // flexDirection: 'row',
+    // justifyContent: 'space-between'
   },
   textTitle: {
-    fontSize: 16
+    fontSize: 16,
   },
   textPrice: {
     fontSize: 16,
-    fontWeight: "bold"
-  }
-})
+    fontWeight: "bold",
+  },
+  optionsContainer: {
+    position: "absolute",
+    right: 0,
+    backgroundColor: colors.white,
+    width: "20%",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    flex: 1,
+  },
+  deleteButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: colors.error,
+  },
+});
---
+++ components/modalDeleteItem.jsx
@@ -0,0 +1,88 @@
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from '../constants/colors'
+import Counter from '../components/counter'
+
+const ModalDeleteItem = ({ modalVisible, item, quantity, handleDelete, handleCancel }) => {
+    return (
+    <Modal visible={modalVisible} animationType="slide" transparent={true}>
+      <View style={styles.modal}>
+        <View style={styles.container}>
+          <Text style={styles.text}>
+            ¿Está seguro que desea eliminar {item}?
+          </Text>
+          {Number(quantity) < 1 && (
+            <Counter />
+          )}
+          <View style={styles.buttonsContainer}>
+            <Pressable onPress={handleDelete} style={styles.presseableDelete}>
+              <Text style={styles.textButtonDelete}>Eliminar</Text>
+            </Pressable>
+            <Pressable onPress={handleCancel} style={styles.presseableCancel}>
+              <Text style={styles.textButtonCancel}>Cancelar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default ModalDeleteItem;
+
+const styles = StyleSheet.create({
+    modal: {
+        backgroundColor: "#cccccc88",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+      },
+      container: {
+        backgroundColor: colors.white,
+        width: "80%",
+        alignItems: "center",
+        borderWidth: 1,
+        borderRadius: 6,
+        borderColor: colors.error,
+        padding: 20,
+        gap: 30
+      },
+      text: {
+        fontSize: 20,
+        textAlign: "center",
+        fontWeight: "bold"
+      },
+      buttonsContainer: {
+        width: "100%",
+        gap: 10,
+        alignItems: "center"
+      },
+      presseableCancel: {
+        backgroundColor: colors.white,
+        borderWidth: 1,
+        borderRadius: 6,
+        borderColor: colors.gray,
+        width: "90%",
+        padding: 15,
+        alignItems: "center"
+      },
+      textButtonCancel: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: colors.gray,
+      },
+      presseableDelete: {
+        backgroundColor: colors.error,
+        borderWidth: 1,
+        borderRadius: 6,
+        borderColor: colors.error,
+        width: "90%",
+        padding: 15,
+        alignItems: "center"
+      },
+      textButtonDelete: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: colors.white,
+      }
+});
